87eee84a0098a3da1405b71ad76e5267f867378a
unknown <lewislewis996@gmail.com>
2022-07-08 13:24:48 +0200

    fix(web/graphql): correct duplicate operation name and generate request hook

--- apps/web/src/generated/graphql.tsx
+++ apps/web/src/generated/graphql.tsx
@@ -17,8 +17,30 @@ export type Scalars = {
 
 export type Mutation = {
   __typename?: 'Mutation';
+  checkQuiz: Scalars['Boolean'];
+  createQuiz?: Maybe<Quiz>;
+  createRound: Round;
   login?: Maybe<User>;
   register: User;
+  updateScore: Round;
+};
+
+
+export type MutationCheckQuizArgs = {
+  id: Scalars['Float'];
+  publicId: Scalars['String'];
+  response: Scalars['Boolean'];
+  score: Scalars['Float'];
+};
+
+
+export type MutationCreateQuizArgs = {
+  publicId: Scalars['String'];
+};
+
+
+export type MutationCreateRoundArgs = {
+  roundType: Scalars['String'];
 };
 
 
@@ -32,14 +54,59 @@ export type MutationRegisterArgs = {
   input: UserInput;
 };
 
+
+export type MutationUpdateScoreArgs = {
+  publicId: Scalars['String'];
+  score: Scalars['Float'];
+};
+
 export type Query = {
   __typename?: 'Query';
   getByUsername?: Maybe<User>;
+  getQuizById?: Maybe<Quiz>;
+  getRoundById?: Maybe<Round>;
 };
 
 
 export type QueryGetByUsernameArgs = {
   username: Scalars['String'];
+};
+
+
+export type QueryGetQuizByIdArgs = {
+  id: Scalars['String'];
+};
+
+
+export type QueryGetRoundByIdArgs = {
+  publicId: Scalars['String'];
+};
+
+export type Quiz = {
+  __typename?: 'Quiz';
+  actorName: Scalars['String'];
+  actorPicture: Scalars['String'];
+  adult: Scalars['Boolean'];
+  createdAt: Scalars['String'];
+  id: Scalars['Int'];
+  movieDescription: Scalars['String'];
+  movieTitle: Scalars['String'];
+  movieUrl: Scalars['String'];
+  originalName: Scalars['String'];
+  quizType: Scalars['Boolean'];
+  releaseDate: Scalars['String'];
+  round: Round;
+  updatedAt: Scalars['String'];
+};
+
+export type Round = {
+  __typename?: 'Round';
+  createdAt: Scalars['String'];
+  publicId: Scalars['String'];
+  quiz: Array<Quiz>;
+  roundType: Scalars['String'];
+  score: Scalars['Float'];
+  updatedAt: Scalars['String'];
 };
 
 export type User = {
@@ -62,6 +129,30 @@ export type UserInput = {
   username: Scalars['String'];
 };
 
+export type CheckQuizMutationVariables = Exact<{
+  score: Scalars['Float'];
+  response: Scalars['Boolean'];
+  publicId: Scalars['String'];
+  checkQuizId: Scalars['Float'];
+}>;
+
+
+export type CheckQuizMutation = { __typename?: 'Mutation', checkQuiz: boolean };
+
+export type CreateQuizMutationVariables = Exact<{
+  publicId: Scalars['String'];
+}>;
+
+
+export type CreateQuizMutation = { __typename?: 'Mutation', createQuiz?: { __typename?: 'Quiz', id: number, adult: boolean, actorName: string, originalName: string, actorPicture: string, movieTitle: string, movieDescription: string, releaseDate: string, movieUrl: string, quizType: boolean, createdAt: string, updatedAt: string, round: { __typename?: 'Round', publicId: string, updatedAt: string, score: number, roundType: string } } | null };
+
+export type CreateRoundMutationVariables = Exact<{
+  roundType: Scalars['String'];
+}>;
+
+
+export type CreateRoundMutation = { __typename?: 'Mutation', createRound: { __typename?: 'Round', publicId: string, score: number, roundType: string, createdAt: string, updatedAt: string } };
+
 export type LoginMutationVariables = Exact<{
   password: Scalars['String'];
   username: Scalars['String'];
@@ -77,6 +168,14 @@ export type RegisterMutationVariables = Exact<{
 
 export type RegisterMutation = { __typename?: 'Mutation', register: { __typename?: 'User', email: string, username: string, createdAt: string, updatedAt: string } };
 
+export type UpdateScoreMutationVariables = Exact<{
+  score: Scalars['Float'];
+  publicId: Scalars['String'];
+}>;
+
+
+export type UpdateScoreMutation = { __typename?: 'Mutation', updateScore: { __typename?: 'Round', publicId: string, score: number, roundType: string, createdAt: string, updatedAt: string } };
+
 export type GetByUsernameQueryVariables = Exact<{
   username: Scalars['String'];
 }>;
@@ -84,7 +183,78 @@ export type GetByUsernameQueryVariables = Exact<{
 
 export type GetByUsernameQuery = { __typename?: 'Query', getByUsername?: { __typename?: 'User', id: number, username: string, email: string, createdAt: string, updatedAt: string } | null };
 
+export type GetQuizByIdQueryVariables = Exact<{
+  getQuizByIdId: Scalars['String'];
+}>;
 
+
+export type GetQuizByIdQuery = { __typename?: 'Query', getQuizById?: { __typename?: 'Quiz', id: number, adult: boolean, actorName: string, originalName: string, actorPicture: string, movieTitle: string, movieDescription: string, releaseDate: string, movieUrl: string, quizType: boolean, createdAt: string, updatedAt: string, round: { __typename?: 'Round', publicId: string } } | null };
+
+export type GetRoundByIdQueryVariables = Exact<{
+  publicId: Scalars['String'];
+}>;
+
+
+export type GetRoundByIdQuery = { __typename?: 'Query', getRoundById?: { __typename?: 'Round', publicId: string, score: number, roundType: string, createdAt: string, updatedAt: string, quiz: Array<{ __typename?: 'Quiz', id: number, adult: boolean, actorName: string, originalName: string, actorPicture: string, movieTitle: string, movieDescription: string, releaseDate: string, movieUrl: string, quizType: boolean, createdAt: string, updatedAt: string }> } | null };
+
+
+export const CheckQuizDocument = gql`
+    mutation CheckQuiz($score: Float!, $response: Boolean!, $publicId: String!, $checkQuizId: Float!) {
+  checkQuiz(
+    score: $score
+    response: $response
+    publicId: $publicId
+    id: $checkQuizId
+  )
+}
+    `;
+
+export function useCheckQuizMutation() {
+  return Urql.useMutation<CheckQuizMutation, CheckQuizMutationVariables>(CheckQuizDocument);
+};
+export const CreateQuizDocument = gql`
+    mutation CreateQuiz($publicId: String!) {
+  createQuiz(publicId: $publicId) {
+    id
+    round {
+      publicId
+      updatedAt
+      score
+      roundType
+    }
+    adult
+    actorName
+    originalName
+    actorPicture
+    movieTitle
+    movieDescription
+    releaseDate
+    movieUrl
+    quizType
+    createdAt
+    updatedAt
+  }
+}
+    `;
+
+export function useCreateQuizMutation() {
+  return Urql.useMutation<CreateQuizMutation, CreateQuizMutationVariables>(CreateQuizDocument);
+};
+export const CreateRoundDocument = gql`
+    mutation CreateRound($roundType: String!) {
+  createRound(roundType: $roundType) {
+    publicId
+    score
+    roundType
+    createdAt
+    updatedAt
+  }
+}
+    `;
+
+export function useCreateRoundMutation() {
+  return Urql.useMutation<CreateRoundMutation, CreateRoundMutationVariables>(CreateRoundDocument);
+};
 export const LoginDocument = gql`
     mutation Login($password: String!, $username: String!) {
   login(username: $username, password: $password) {
@@ -115,6 +285,21 @@ export const RegisterDocument = gql`
 export function useRegisterMutation() {
   return Urql.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument);
 };
+export const UpdateScoreDocument = gql`
+    mutation UpdateScore($score: Float!, $publicId: String!) {
+  updateScore(score: $score, publicId: $publicId) {
+    publicId
+    score
+    roundType
+    createdAt
+    updatedAt
+  }
+}
+    `;
+
+export function useUpdateScoreMutation() {
+  return Urql.useMutation<UpdateScoreMutation, UpdateScoreMutationVariables>(UpdateScoreDocument);
+};
 export const GetByUsernameDocument = gql`
     query GetByUsername($username: String!) {
   getByUsername(username: $username) {
@@ -129,4 +314,58 @@ export const GetByUsernameDocument = gql`
 
 export function useGetByUsernameQuery(options: Omit<Urql.UseQueryArgs<GetByUsernameQueryVariables>, 'query'>) {
   return Urql.useQuery<GetByUsernameQuery>({ query: GetByUsernameDocument, ...options });
+};
+export const GetQuizByIdDocument = gql`
+    query GetQuizById($getQuizByIdId: String!) {
+  getQuizById(id: $getQuizByIdId) {
+    id
+    round {
+      publicId
+    }
+    adult
+    actorName
+    originalName
+    actorPicture
+    movieTitle
+    movieDescription
+    releaseDate
+    movieUrl
+    quizType
+    createdAt
+    updatedAt
+  }
+}
+    `;
+
+export function useGetQuizByIdQuery(options: Omit<Urql.UseQueryArgs<GetQuizByIdQueryVariables>, 'query'>) {
+  return Urql.useQuery<GetQuizByIdQuery>({ query: GetQuizByIdDocument, ...options });
+};
+export const GetRoundByIdDocument = gql`
+    query GetRoundById($publicId: String!) {
+  getRoundById(publicId: $publicId) {
+    publicId
+    quiz {
+      id
+      adult
+      actorName
+      originalName
+      actorPicture
+      movieTitle
+      movieDescription
+      releaseDate
+      movieUrl
+      quizType
+      createdAt
+      updatedAt
+    }
+    score
+    roundType
+    createdAt
+    updatedAt
+  }
+}
+    `;
+
+export function useGetRoundByIdQuery(options: Omit<Urql.UseQueryArgs<GetRoundByIdQueryVariables>, 'query'>) {
+  return Urql.useQuery<GetRoundByIdQuery>({ query: GetRoundByIdDocument, ...options });
 };
